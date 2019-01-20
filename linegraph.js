0266fbdeb090c455e5c3fb47c01346e4cecb58ff
parthkothare <parthkothare@gmail.com>
2019-01-20 17:02:02 +0530

    Add files via upload

---
+++ linegraph.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  $.ajax({
+    url : "http://localhost/chartjs/followersdata.php",
+    type : "GET",
+    success : function(data){
+      console.log(data);
+
+      var srno = [];
+      var emotion = [];
+      var twitter_follower = [];
+      var googleplus_follower = [];
+
+      for(var i in data) {
+        srno.push("UserID " + data[i].srnoi);
+        emotion.push(data[i].emotionid);
+      }
+
+      var chartdata = {
+        labels: srno,
+        datasets: [
+          {
+            label: "anger",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(59, 89, 152, 0.75)",
+            borderColor: "rgba(59, 89, 152, 1)",
+            pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
+            pointHoverBorderColor: "rgba(59, 89, 152, 1)",
+            data: facebook_follower
+          },
+          {
+            label: "surprise",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(29, 202, 255, 0.75)",
+            borderColor: "rgba(29, 202, 255, 1)",
+            pointHoverBackgroundColor: "rgba(29, 202, 255, 1)",
+            pointHoverBorderColor: "rgba(29, 202, 255, 1)",
+            data: twitter_follower
+          },
+          {
+            label: "happy",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(211, 72, 54, 0.75)",
+            borderColor: "rgba(211, 72, 54, 1)",
+            pointHoverBackgroundColor: "rgba(211, 72, 54, 1)",
+            pointHoverBorderColor: "rgba(211, 72, 54, 1)",
+            data: googleplus_follower
+          }
+        ]
+      };
+
+      var ctx = $("#mycanvas");
+
+      var LineGraph = new Chart(ctx, {
+        type: 'line',
+        data: chartdata
+      });
+    },
+    error : function(data) {
+
+    }
+  });
+});
